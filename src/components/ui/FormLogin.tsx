da6a6f7e307d7ac024c88665fe7beca6de5fa87d
v4leng <valem.g_13@hotmail.com>
2023-09-27 15:59:57 -0300

    validations-login

--- src/components/ui/FormLogin.tsx
+++ src/components/ui/FormLogin.tsx
@@ -8,65 +8,71 @@ import {
   UserIcon,
 } from "@heroicons/react/24/outline";
 import Button from "./Button";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 interface Props {
   children: ReactNode;
 }
 
 const FormLogin = ({ children }: Props) => {
-  const [errors, setErrors] = useState<string[]>([]);
-  const [email, setEmail] = useState<string>("");
-  const [showPassword, setShowPassword] = useState(false);
-  const [password, setPassword] = useState<string>("");
-
   const router = useRouter();
+
+  // Define el esquema de validación Yup
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("El correo electrónico no es válido")
+      .required("El correo electrónico es requerido"),
+    password: Yup.string()
+      .min(6, "La contraseña debe tener al menos 6 caracteres")
+      .required("La contraseña es requerida"),
+  });
+
+  // Define el formulario con Formik
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      const responseNextAuth = await signIn("credentials", {
+        email: values.email,
+        password: values.password,
+        redirect: false,
+      });
+
+      router.push("/start-delivery");
+    },
+  });
+
+  const { values, touched, errors, handleChange, handleBlur, handleSubmit } =
+    formik;
+
+  const [showPassword, setShowPassword] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setErrors([]);
-
-    const responseNextAuth = await signIn("credentials", {
-      email,
-      password,
-      redirect: false,
-    });
-
-    console.log(responseNextAuth);
-
-    if (responseNextAuth?.error) {
-      setErrors(responseNextAuth.error.split(","));
-      return;
-    }
-
-    router.push("/start-delivery");
-  };
-
   return (
     <div className="text-center w-full items-center text-sm">
-      <form className="rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+      <form
+        className="rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={formik.handleSubmit}
+      >
         <div className="mb-8 flex items-center justify-center md:justify-start">
           <div className="max-w-[300px]">
             <LogoIcon />
           </div>
         </div>
-        <div className="flex items-center appearance-none bg-inherit border border-white rounded-lg py-0 px-3 text-white mb-3 leading-tight focus:outline-none h-[45px] w-full">
+        <div className=" flex items-center appearance-none bg-inherit border border-white rounded-lg py-0 px-3 text-white leading-tight focus:outline-none h-[45px] w-full">
           <UserIcon className="h-5 w-5 text-white" />
           <input
-            value={email}
-            onChange={handleEmailChange}
-            className="pl-2 outline-none border-none bg-inherit w-full placeholder-white"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            className=" pl-2 outline-none border-none bg-inherit w-full placeholder-white"
             type="text"
             name="email"
             id="email"
@@ -74,7 +80,10 @@ const FormLogin = ({ children }: Props) => {
             required
           />
         </div>
-        <div className="flex items-center appearance-none bg-inherit border border-white rounded-lg py-0 px-3 text-white mb-5 leading-tight focus:outline-none h-[45px] w-full">
+        {touched.email && errors.email ? (
+          <div className="text-red-500 text-xs">{errors.email}</div>
+        ) : null}
+        <div className="flex items-center mt-3  appearance-none bg-inherit border border-white rounded-lg py-0 px-3 text-white  leading-tight focus:outline-none h-[45px] w-full">
           {showPassword ? (
             <LockOpenIcon
               className="h-5 w-5 text-white cursor-pointer"
@@ -92,12 +101,19 @@ const FormLogin = ({ children }: Props) => {
             name="password"
             id="password"
             placeholder={showPassword ? "Contraseña" : "**********"}
-            value={password}
-            onChange={handlePasswordChange}
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
             required
           />
         </div>
-        <div className="mb-4">
+        <>
+          {touched.password && errors.password ? (
+            <div className="text-red-500 text-xs mt-0 ">{errors.password}</div>
+          ) : null}
+        </>
+
+        <div className="mb-4 mt-5">
           <Button
             text="Ingresar"
             className="bg-secondary text-primary text-sm"
