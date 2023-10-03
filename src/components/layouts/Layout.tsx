6cad90c66715ec11b8158b5e3bcfa747561c77ca
v4leng <valem.g_13@hotmail.com>
2023-10-02 21:04:07 -0300

    protected-routes

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,6 +1,9 @@
 import React, { ReactNode } from "react";
 import Navbar from "../ui/Navbar";
 import { SessionProvider } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 interface Props {
   children: ReactNode;
@@ -9,17 +12,34 @@ interface Props {
 }
 
 const Layout = ({ children, title, className }: Props) => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status !== "authenticated") {
+      router.push("/login");
+    }
+  }, [status, router]);
+
   return (
     <>
-      <title>{title || "fazt&punctuals"}</title>
-      <SessionProvider>
-        <Navbar></Navbar>
-        <main
-         className={`${className} container mx-auto flex items-center justify-center p-10 xl:p-24 lg:p-18 md:p-16`}
-        >
-          {children}
-        </main>
-      </SessionProvider>
+      {status === "authenticated" ? (
+        <>
+          <title>{title || "fazt&punctuals"}</title>
+          <SessionProvider>
+            <Navbar></Navbar>
+            <main
+              className={`${className} container mx-auto flex items-center justify-center p-10 xl:p-24 lg:p-18 md:p-16`}
+            >
+              {children}
+            </main>
+          </SessionProvider>
+        </>
+      ) : (
+        <>
+          <img src="/error.jpg" alt="Error" />
+        </>
+      )}
     </>
   );
 };
